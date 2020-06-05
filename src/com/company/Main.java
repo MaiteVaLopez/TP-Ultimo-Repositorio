15ba2a0eead265110b37d7cdcc9ee43a599b4121
agusttinv <agusttinv@gmail.com>
2020-06-04 21:50:21 -0300

    cambiossssss agus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
-        System.out.println("Hola! Tratando de hacer andar git jeeeeeeejkujnmbnmkjeeeeeee5345eeeeeeee");
+        System.out.println("Si no anda me muero");
 
-        System.out.println("Probando subir proyecto");
     }
 }
